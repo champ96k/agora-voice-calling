f842fba5501136a0ca6e2196527f5428c7668c5d
Tushar Nikam <champ96k@gmail.com>
2021-07-11 20:47:43 +0530

    code refactor

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,4 +1,4 @@
-export 'package:pustack_assignment/core/cubit/voice_caller_cubit.dart';
-export 'package:pustack_assignment/core/constants/constant_colors.dart';
-export 'package:pustack_assignment/core/constants/constants_images.dart';
-export 'package:pustack_assignment/core/app_configs/routes_generator.dart';
+export 'package:voice_calling/core/cubit/voice_caller_cubit.dart';
+export 'package:voice_calling/core/constants/constant_colors.dart';
+export 'package:voice_calling/core/constants/constants_images.dart';
+export 'package:voice_calling/core/app_configs/routes_generator.dart';
